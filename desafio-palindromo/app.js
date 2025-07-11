3bb8b030cf5c89e4e26cd9ecd84d6bd48e1b4912
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2025-07-10 22:18:24 -0300

    feat: add game rental functionality with status confirmation and display updates

---
+++ desafio-palindromo/app.js
@@ -0,0 +1,13 @@
+essaPalavraEUmPalindromo("radar");
+
+function essaPalavraEUmPalindromo(palavra) {
+    // var palavra = "rever";
+    var separandoAsLetrasDaPalavra = palavra.split("");
+    var palavraInvertida = separandoAsLetrasDaPalavra.reverse();
+    palavraInvertida = palavraInvertida.join("");
+    if (palavra === palavraInvertida) {
+        console.log(`A palavra ${palavra} é um palíndromo.`);
+    } else {
+        console.log(`A palavra ${palavra} não é um palíndromo.`);
+    }
+}
