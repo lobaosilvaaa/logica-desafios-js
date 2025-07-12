615171ce4ed201af25b580126ce058a986e81ecf
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2025-07-12 10:20:37 -0300

    feat: refactor product addition logic in shopping cart for improved validation and subtotal calculation

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,16 +1,44 @@
+let totalGeral = 0;
+limpar();
+
 function adicionar() {
     // Recuperar valores: nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-    
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert('Por favor, selecione um produto válido.');
+        return;
+    }
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
+    let nomeProduto = produto.split(' - ')[0];
+    let valorUnitario = produto.split('R$')[1];
 
     // Calcular o preco, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+
     // Adicionar o produto ao carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+                    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+                </section>`;
+
     // Atualizar o total do carrinho
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
