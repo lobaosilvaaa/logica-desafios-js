a2bf29906ff85de42cbdd6cae180464fde1d4449
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2025-07-12 09:03:36 -0300

    feat: enhance shopping cart functionality with product addition and clearing methods

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,16 @@
+function adicionar() {
+    // Recuperar valores: nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+    // Calcular o preco, o nosso subtotal
+    // Adicionar o produto ao carrinho
+    // Atualizar o total do carrinho
+}
+
+function limpar() {
+
+}
