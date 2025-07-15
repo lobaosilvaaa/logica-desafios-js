2597e5ceeba217793bd56da4447695469e48e5da
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2025-07-14 21:27:43 -0300

    feat: refactor adicionar and sortear functions for improved clarity and functionality

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -3,6 +3,7 @@ let amigos =[];
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+
     amigos.push(amigo.value);
     
     if (lista.textContent == '') {
@@ -12,10 +13,28 @@ function adicionar() {
     }
 
     amigo.value = '';
+
+    atualizarLista();
+    atualizarSorteio();
 }
 
 function sortear() {
-    embaralha(amigos);
+    embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML += `${amigos[i]} --> ${amigos[0]} <br>`;
+        } else {
+            sorteio.innerHTML += `${amigos[i]} --> ${amigos[i + 1]} <br>`;
+        }
+    }
+}
+
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
 }
 
 function embaralhar(lista) {
@@ -23,4 +42,34 @@ function embaralhar(lista) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
         [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
